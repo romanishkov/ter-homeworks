98a8ee181b03d18cd804e487070bce9563b786f2
romanishkov <ishkov.roma@gmail.com>
2024-03-04 22:02:50 +0300

    05task2

--- 04/src/providers.tf
+++ 04/src/providers.tf
@@ -5,6 +5,17 @@ terraform {
     }
   }
   required_version = ">=0.13"
+
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket = "tfstate-study"
+    region = "ru-central1"
+    key = "terraform.tfstate"
+    skip_region_validation = true
+    skip_credentials_validation = true
+    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gs1a52p2iupn9pd225/etnn599fe8vnqnb4r7g4"
+    dynamodb_table = "tfstate-study"
+  }
 }
 
 provider "yandex" {
